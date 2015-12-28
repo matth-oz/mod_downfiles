3b4858adb5d98b23f8274a679db40bbd35cdf098
Matvey Zemskov <matth_oz@mail.ru>
2015-12-28 16:43:26 +0300

    add CSS-styles

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,14 +1,21 @@
 <?php
 defined('_JEXEC') or die;
+$document = JFactory::getDocument();
+$document->addStylesheet('/modules/mod_downfiles/media/css/style.css');
 ?>
 
 <?php if(!empty($list)):?>
 <div class="downfiles<?php echo $moduleclass_sfx ?>">
+    <?php if(!empty($text_before)):?>
+    <div class="downfiles-txtbefore">
+        <?php echo $text_before;?>
+    </div>
+    <?php endif;?>
     <ul class="downfiles-list">
         <?php foreach($list as $item):?>
         <?php $ext = substr(strrchr($item->filename, '.'), 1);?>
         <li>
-            <a href="index.php?option=com_downfiles&task=item.getfile&file_id=<?php echo $item->id?>"><?php echo $item->file_desc?></a> (<?php echo $ext.", ".$item->file_size;?>)
+            <a href="index.php?option=com_downfiles&task=item.getfile&file_id=<?php echo $item->id?>"><?php echo $item->file_desc?></a> <span>(<?php echo $ext.", ".$item->file_size;?>)</span>
         </li> 
         <?php endforeach;?>
     </ul>
